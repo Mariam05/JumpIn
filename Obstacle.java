05efc96a8dcb88b0d58f04a5417bc6c610a89840
nazifatanzim <44646521+nazifatanzim@users.noreply.github.com>
2019-10-16 14:45:30 -0400

    WIP added some methods

--- Obstacle.java
+++ Obstacle.java
@@ -4,5 +4,53 @@
  *
  */
 public class Obstacle {
-	// TODO define and implement methods
+	// TODO enums
+	private int xPosition, yPosition;
+	
+	public Obstacle(int x, int y) {
+		this.xPosition = x;
+		this.yPosition = y;
+	}
+	/*
+	private enum Type{
+		
+	}
+	
+	private enum Rabbit{
+		
+	}
+	
+	private enum Fox{
+		
+	}
+	/**
+	 * 
+	 */
+	public void move(int newX, int newY) {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void getType(){
+	}
+	
+	/**
+	 * 
+	 */
+	private void setType() {
+		
+	}
+	
+	public String toString() {
+		return "(" + this.xPosition + "," + this.yPosition + ")";
+	}
+	/*
+	 * main class to test Obstacle class methods
+	 * TODO remove before submission
+	public static void main(String args[]) {
+		Obstacle o = new Obstacle(1,2);
+		System.out.println(o.toString());
+	}*/
 }
