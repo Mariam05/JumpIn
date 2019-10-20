9352825ef97a04c7786628fd4b14625eddb18d29
Hassan Hassan <47644207+hassanidea@users.noreply.github.com>
2019-10-20 10:01:57 -0500

    Update Client.java

--- Client.java
+++ Client.java
@@ -47,7 +47,7 @@ public class Client {
 			
 		}
 		
-		System.out.println("Conragulations, you have completed the game");
+		System.out.println("Congragulations, you have completed the game");
 		
 
 	}
