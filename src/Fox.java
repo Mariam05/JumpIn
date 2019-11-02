2f7569ce2c651fd6d6eea382a42f46946cfba70e
Mariam05 <tomariam@gmail.com>
2019-11-02 19:07:51 -0400

    All final classes here, though some not implemented

---
+++ src/Fox.java
@@ -0,0 +1,20 @@
+
+public class Fox extends Piece{
+
+	protected Fox(String pieceName) {
+		super(pieceName, Type.FOX);
+	}
+
+	@Override
+	public boolean validateMove(int currPos, int destinationPos) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/Rabbit.java
@@ -0,0 +1,21 @@
+
+public class Rabbit extends Piece {
+
+	protected Rabbit(String pieceName) {
+		super(pieceName, Type.RABBIT);
+	}
+
+	@Override
+	public boolean validateMove(int currPos, int destinationPos) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
