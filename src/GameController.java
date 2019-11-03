98891fe4cda77bbb3c2bb48477411a6d22b45cbd
Mariam05 <tomariam@gmail.com>
2019-11-03 15:05:33 -0500

    Implementing controller

--- src/GameController.java
+++ src/GameController.java
@@ -6,5 +6,12 @@
 public class GameController {
 
 	private Game game;
+	private GameView gameView;
+	
+	public GameController(Game game, GameView gameView) {
+		this.game = game;
+		this.gameView = gameView;
+	}
+	
 	
 }
