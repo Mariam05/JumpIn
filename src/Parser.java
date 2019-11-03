5348b765cd49f70835ac620793a52edc3a4d9d29
Mariam05 <tomariam@gmail.com>
2019-11-02 23:50:46 -0400

    Trying to fix merge conflict

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
 /**
@@ -66,72 +65,3 @@ public class Parser {
     }
 
 }
-=======
-import java.util.Scanner;
-
-/**
- * This parser reads user input and tries to interpret it as a command. 
- * Every time it is called it reads a line from the terminal and
- * tries to interpret the line as a three part command. It returns the command
- * as an object of class Command.
- *
- * The parser has a set of known command words. It checks user input against
- * the known commands, and if the input is not one of the known commands, it
- * returns a command object that is marked as an unknown command.
- * 
- * @author Mariam Almalki
- *
- */
-public class Parser {
-	
-	 private CommandWord commands;  // holds all valid command words
-	 private Scanner reader;         // source of command input
-	    
-    /**
-     * Create a parser to read from the terminal window.
-     */
-    public Parser() 
-    {
-        commands = new CommandWord();
-        reader = new Scanner(System.in);
-    }
-
-    /**
-     * @return The next command from the user.
-     */
-    public Command getCommand() 
-    {
-        String inputLine;   // will hold the full input line
-        String part1 = null;
-        String part2 = null;
-        String part3 = null;
-
-        System.out.println("> ");     // print prompt
-
-        inputLine = reader.nextLine();
-
-        // Find up to two words on the line.
-        Scanner tokenizer = new Scanner(inputLine);
-        if(tokenizer.hasNext()) {
-            part1 = tokenizer.next();      // get first part of player input (represents command)
-            if(tokenizer.hasNext()) {
-                part2 = tokenizer.next();      // get second part of player input (represents piece to use)
-                if (tokenizer.hasNext()) {
-                	part3 = tokenizer.next();  //get third part of player input (represents position to move)
-                	// note: we just ignore the rest of the input line.
-                }
-            }
-        }
-
-        // Now check whether this word is known. If so, create a command
-        // with it. If not, create a "null" command (for unknown command).
-        if(commands.isCommand(part1)) {
-            return new Command(part1, part2, part3);
-        }
-        else {
-            return new Command(null, part2, part3); 
-        }
-    }
-
-}
->>>>>>> ada47d1d590091184cd420e0e49786311fcb0bcf
