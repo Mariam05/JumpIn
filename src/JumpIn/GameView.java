6cb1bd815384299a45575d4dfd6bf7058875c0ba
Taher Shabaan <55097549+TaherShabaan99@users.noreply.github.com>
2019-11-30 22:46:02 -0500

    Add files via upload
    Here is the new GameView

--- src/JumpIn/GameView.java
+++ src/JumpIn/GameView.java
@@ -7,9 +7,9 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.Toolkit;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.Serializable;
 import java.util.HashMap;
 
 import javax.swing.BorderFactory;
@@ -23,8 +23,6 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.KeyStroke;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 
@@ -35,18 +33,18 @@ import javax.swing.JSplitPane;
  * @author Taher Shabaan, Hassan Hassan, Mariam Almalki, Nazifa Tanzim
  *
  */
-public class GameView extends JFrame {
+public class GameView extends JFrame implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Container container;
-	private JPanel startPage, levelsPage, currentPage;
-	JButton board[][], levels[][], newGameBtn, startBtn, loadGameBtn, levelBtn;// This will be a board of squares
-	JButton createBoardBtn;
+	private JPanel startPage, levelsPage;
+	private JButton newGameBtn, loadGameBtn;
 	private JScrollPane defaultLevelsPane, customLevelsPane;
+	JButton board[][]; // This will be a board of squares
 
 	private int size; // The size of the board
 
-	public Image piece, whiteRabbit, yellowRabbit, greyRabbit, mushroom, foxface, foxtail, hole;
+	private Image piece, whiteRabbit, yellowRabbit, greyRabbit, mushroom, foxface, foxtail, hole;
 
 	/**
 	 * this will be used twice firstly to add holes and secondly to change the
@@ -56,13 +54,12 @@ public class GameView extends JFrame {
 
 	private Game game;
 	private JMenuBar menuBar;
-	private JMenuItem menuItemHelp, menuItemQuit, menuItemReset, menuItemUndo, menuItemRedo, menuItemHint;
-
+	private JMenuItem menuItemHelp, menuItemQuit, menuItemReset, menuItemUndo, menuItemRedo, menuItemHint, menuItemSave, menuItemLoad;
+	
 	// These will hold the names of all the levels
 	private static JList<String> defaultLevels, customLevels;
-	private static DefaultListModel<String> defaultList = new DefaultListModel<>(),
-			customList = new DefaultListModel<>();
-
+	private static DefaultListModel<String> defaultList = new DefaultListModel<>(), customList = new DefaultListModel<>();
+	
 	/**
 	 * Create a new view
 	 * 
@@ -74,12 +71,10 @@ public class GameView extends JFrame {
 		this.game = model;
 
 		size = model.getBoard().SIZE;
-		board = new JButton[size][size];
 
 		// Creating start page
-		createStartPage();
-		currentPage = startPage;
-
+		createStartPage();		
+		
 		container = new Container();
 		container.setLayout(new GridLayout(size, size));
 
@@ -90,16 +85,16 @@ public class GameView extends JFrame {
 		setVisible(true);
 
 	}
-
+	
 	/**
-	 * Switches from levels page to game
+	 * Switches from levels page to game 
 	 * 
 	 */
 	public void goToGame() {
 		remove(levelsPage);
 		displayGame();
 	}
-
+	
 	/**
 	 * Switches from start page to levels page
 	 */
@@ -107,7 +102,6 @@ public class GameView extends JFrame {
 		remove(startPage);
 		initializeDefaultLevels();
 		createLevelsPage();
-		currentPage = levelsPage;
 	}
 
 	/**
@@ -116,11 +110,11 @@ public class GameView extends JFrame {
 	private void displayGame() {
 		add(container); // add the container to the jframe
 		addMenuItems();
-		createButtonsBoard(board);
+		createBoard();
 		instantiateIcons();
 		putIconsOnBoard();
 	}
-
+	
 	/**
 	 * Creating start page
 	 */
@@ -129,52 +123,51 @@ public class GameView extends JFrame {
 		startPage.setLayout(null);
 		startPage.setBackground(new Color(0, 204, 0));
 		add(startPage);
-
+		
 		// Adding button to go to levels page
 		newGameBtn = new JButton("Start a New Game");
 		newGameBtn.setBounds(250, 350, 210, 50);
-		newGameBtn.setFont(new Font("Monospaced", Font.BOLD, 20));
+		newGameBtn.setFont(new Font ("Monospaced", Font.BOLD, 20));
 		newGameBtn.setBackground(Color.WHITE);
 		newGameBtn.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 		newGameBtn.setForeground(Color.BLUE);
-
+		
 		// Adding button to load previous game
 		loadGameBtn = new JButton("Load Previous Game");
 		loadGameBtn.setBounds(230, 450, 250, 50);
-		loadGameBtn.setFont(new Font("Monospaced", Font.BOLD, 20));
+		loadGameBtn.setFont(new Font ("Monospaced", Font.BOLD, 20));
 		loadGameBtn.setBackground(Color.WHITE);
 		loadGameBtn.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 		loadGameBtn.setForeground(Color.BLUE);
+		
+		//When the user will press save so we'll create a SaveData object and pass this as we want the user
+		//to save this view then after that call the save method in ResourceManager class and pass the save object and fileName
+		loadGameBtn.addActionListener(event -> {
+			try {
+				SaveData data = (SaveData) ResourceManager.load("levelSaved");//levelSaved is the file that will be loaded and don't use xxx/levelSaved
+			}
+			catch (Exception e) {
+				System.out.println("Couldn't load: " + e.getMessage());
+			}
+		});
 
-		// code for if we want the create board button to be on the start page.
-//		createBoardBtn = new JButton("Create a Puzzle");
-//		createBoardBtn.setBounds(230, 550, 250, 50);
-//		createBoardBtn.setFont(new Font ("Monospaced", Font.BOLD, 20));
-//		createBoardBtn.setBackground(Color.WHITE);
-//		createBoardBtn.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-//		createBoardBtn.setForeground(Color.BLUE);
-		// startPage.add(createBoardBtn);
-		// createBoardBtn.addActionListener(e -> {remove(startPage); add(new
-		// LevelBuilderPanel(size)); setVisible(true);});
-
+		
 		// Adding game title
 		JLabel title = new JLabel("JUMP IN");
-		title.setFont(new Font("Monospaced", Font.BOLD, 125));
+		title.setFont(new Font ("Monospaced", Font.BOLD, 125));
 		title.setBounds(70, 100, 900, 100);
 		title.setForeground(Color.WHITE);
-
+		
 		// Adding all components to panel
 		startPage.add(title);
 		startPage.add(newGameBtn);
 		startPage.add(loadGameBtn);
-
-		loadGameBtn.addActionListener(e -> {
-			displayMessage("load saved game");
-		});
-
+		
+		loadGameBtn.addActionListener(e -> {displayMessage("load saved game");});
+		
 	}
-
-	/**
+	
+	/** 
 	 * creating levels page
 	 * 
 	 */
@@ -184,69 +177,59 @@ public class GameView extends JFrame {
 		levelsPage.setLayout(new BorderLayout());
 		levelsPage.setBackground(new Color(0, 204, 0));
 		add(levelsPage);
-
+		
 		// Adding headers for the different levels options
 		JPanel headers = new JPanel();
 		levelsPage.add(headers, BorderLayout.NORTH);
 
 		JLabel defaultLvl = new JLabel("Default Levels        ");
-		defaultLvl.setFont(new Font("Monospaced", Font.BOLD, 20));
+		defaultLvl.setFont(new Font ("Monospaced", Font.BOLD, 20));
 		headers.add(defaultLvl);
 
 		JLabel customLvl = new JLabel("|      Custom Levels   ");
-		customLvl.setFont(new Font("Monospaced", Font.BOLD, 20));
+		customLvl.setFont(new Font ("Monospaced", Font.BOLD, 20));
 		headers.add(customLvl);
-
-		// Adding buttons to navigate the page
+		
+		//Adding buttons to navigate the page
 		JPanel buttons = new JPanel();
 		buttons.setLayout(new GridLayout(1, 2));
-		levelsPage.add(buttons, BorderLayout.SOUTH);
+		levelsPage.add(buttons, BorderLayout.SOUTH);		
 
 		JButton backBtn = new JButton("Back");
-		backBtn.addActionListener(e -> {
-			defaultList.clear();
-			dispose();
-			Main.main(null);
-		});
-		buttons.add(backBtn);
+		backBtn.addActionListener(e -> {defaultList.clear();dispose(); Main.main(null);});
+		buttons.add(backBtn);		
 
-		startBtn = new JButton("Start");
-		buttons.add(startBtn);
+		JButton start = new JButton("Start");
+		start.addActionListener(e -> {goToGame();});
+		buttons.add(start);		
 
 		JButton buildLvlBtn = new JButton("Build A Level");
 		buttons.add(buildLvlBtn);
 		
-
 		// Adding the levels lists to scroll panes
 		JScrollPane pane1 = new JScrollPane(defaultLevels);
 		JScrollPane pane2 = new JScrollPane(customLevels);
 		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, pane1, pane2);
-
+		
 		splitPane.setDividerLocation(345);
 		splitPane.setBounds(0, 30, 700, 600);
 		levelsPage.add(splitPane, BorderLayout.CENTER);
-	
-		buildLvlBtn.addActionListener(e -> {
-			remove(levelsPage);
-			add(new LevelBuilderPanel(size));
-			setVisible(true);
-		});
-
+		
 		setVisible(true);
 	}
-
+	
 	/**
 	 * Initializes the list of levels
 	 */
 	private void initializeDefaultLevels() {
-		for (int i = 1; i <= 6; i++) {
+		for(int i = 1; i <= 6; i++) {
 			defaultList.addElement("Level " + i);
 		}
-
+		
 		defaultLevels = new JList<>(defaultList);
 		customLevels = new JList<>(customList);
 	}
-
+	
 	/**
 	 * Get the size of the view's board
 	 * 
@@ -284,25 +267,25 @@ public class GameView extends JFrame {
 				if (r.getColour() == Color.YELLOW)
 					piece = yellowRabbit.getScaledInstance(110, 110, java.awt.Image.SCALE_SMOOTH);
 				board[pieceRow][pieceCol].setIcon(new ImageIcon(piece)); // add the image to the animal's location
-
+				
 			} else if (p instanceof Fox) { // if it's a fox, associate it with the appropriate fox image depending
-											// on whether it's a head/tail
+												// on whether it's a head/tail
 				Fox f = (Fox) p;
-
+				
 				if (f.isHead()) {
 					Fox ft = f.getAssociatedPart();
-
+					
 					piece = foxface.getScaledInstance(110, 110, java.awt.Image.SCALE_SMOOTH);
 					board[pieceRow][pieceCol].setIcon(new ImageIcon(piece)); // add the image to the animal's location
 
 					piece = foxtail.getScaledInstance(110, 110, java.awt.Image.SCALE_SMOOTH);
 					board[ft.getYPos()][ft.getXPos()].setIcon(new ImageIcon(piece)); // add the image to the animal's
-																						// location
+																					 // location
 				}
 			} else if (p instanceof Mushroom) {// Add the mushrooms
 				piece = mushroom.getScaledInstance(110, 110, java.awt.Image.SCALE_SMOOTH);
 				board[pieceRow][pieceCol].setIcon(new ImageIcon(piece));
-
+				
 			}
 		}
 	}
@@ -314,63 +297,64 @@ public class GameView extends JFrame {
 		// Create menu bar
 		menuBar = new JMenuBar();
 
-		final int SHORTCUT_MASK = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask(); // to save typing
-
 		// Add undo button
 		menuItemUndo = new JMenuItem("Undo");
 		menuItemUndo.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
-		menuItemUndo.addActionListener(e -> {
-			undo();
-		});
+		menuItemUndo.addActionListener(e -> {undo();});
 		menuBar.add(menuItemUndo);
 
 		// Add redo button
 		menuItemRedo = new JMenuItem("Redo");
 		menuItemRedo.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
-		menuItemRedo.addActionListener(e -> {
-			redo();
-		});
+		menuItemRedo.addActionListener(e -> {redo();});
 		menuBar.add(menuItemRedo);
 
 		// Add help button
 		menuItemHelp = new JMenuItem("Help");
 		menuItemHelp.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
-		menuItemHelp.addActionListener(e -> {
-			displayMessage(game.printGameInstructions());
-		});
+		menuItemHelp.addActionListener(e -> {displayMessage(game.printGameInstructions());});
 		menuBar.add(menuItemHelp);
 
 		// TODO fix it so that it only resets the level and not the entire game
-		// potentially do this via serialization i.e. store initial state and retrieve
-		// it when necessary
-
+		// potentially do this via serialization i.e. store initial state and retrieve it when necessary
+		
 		// Add reset button
 		menuItemReset = new JMenuItem("Reset");
 		menuItemReset.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
-		menuItemReset.addActionListener(e -> {
-			dispose();
-			Main.main(null);
-		});
+		menuItemReset.addActionListener(e -> {dispose(); Main.main(null);});
 		menuBar.add(menuItemReset);
 
 		// Add quit button
 		menuItemQuit = new JMenuItem("Quit");
 		menuItemQuit.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
-		menuItemQuit.addActionListener(e -> {
-			displayMessage(game.quitMessage());
-			dispose();
-		});
+		menuItemQuit.addActionListener(e -> {displayMessage(game.quitMessage()); dispose();});
 		menuBar.add(menuItemQuit);
 
-		// Add hint button
+		//Add hint button
 		menuItemHint = new JMenuItem("Hint", KeyEvent.VK_H);
 		menuItemHint.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
-		menuItemHint.addActionListener(e -> {
-			displayHint((new Solver(game)).getHint());
-		});
-		menuItemHint.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, SHORTCUT_MASK));
+		menuItemHint.addActionListener(e -> {displayHint((new Solver(game)).getHint());});
 		menuBar.add(menuItemHint);
-
+		
+		//Add save BUTTON
+		
+		menuItemSave = new JMenuItem("Save");
+		menuItemSave.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
+		//When the user will press save so we'll create a SaveData object and pass this as we want the user
+		//to save this view then after that call the save method in ResourceManager class and pass the save object and fileName
+		menuItemSave.addActionListener(event -> {
+			SaveData data = new SaveData(game);
+			try {
+				ResourceManager.save(data,"levelSaved");//levelSaved is the file and don't use xxx/levelSaved
+			}
+			catch (Exception e) {
+				System.out.println("Couldn't save: " + e.getMessage());
+			}
+		});
+		menuBar.add(menuItemSave);
+		
+		
+		
 		add(menuBar, BorderLayout.NORTH);
 	}
 
@@ -378,7 +362,8 @@ public class GameView extends JFrame {
 	 * The board will be created then we are adding the buttons in side it and also
 	 * adding background color
 	 */
-	private void createButtonsBoard(JButton[][] board) {
+	private void createBoard() {
+		board = new JButton[size][size];
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
 				JButton button = new JButton();
@@ -415,7 +400,7 @@ public class GameView extends JFrame {
 		// instantiate the fox face image
 		foxface = new ImageIcon(this.getClass().getResource("/JumpIn/Icons/foxface.png")).getImage();
 
-		// instantiate the fox tail image
+		// instantiate the fox tail image 
 		foxtail = new ImageIcon(this.getClass().getResource("/JumpIn/Icons/foxtail.png")).getImage();
 	}
 
@@ -432,27 +417,28 @@ public class GameView extends JFrame {
 		}
 		putIconsOnBoard();
 	}
-
+	
 	/**
-	 * Displays the hint on the board
+	 * Displays the hint on the board 
 	 * 
 	 * @param c
 	 */
 	public void displayHint(Command c) {
 		// Highlights the piece to be moved
-		board[game.getPieceFromCommand(c).getYPos()][game.getPieceFromCommand(c).getXPos()].setBackground(Color.BLUE);
-
+		board[game.getPieceFromCommand(c).getYPos()][game.getPieceFromCommand(c).getXPos()]
+				.setBackground(Color.BLUE);
+		
 		// Highlights piece destination
-		board[c.getY()][c.getX()].setBackground(Color.BLUE);
+		board[c.getY()][c.getX()].setBackground(Color.BLUE); 
 	}
-
+	
 	/**
 	 * Undoes current move and stores command to revert it in redo
 	 * 
 	 * @author Nazifa Tanzim
 	 */
 	private void undo() {
-		if (!game.undo()) {
+		if(!game.undo()) {
 			displayMessage("No more undo's left");
 		} else {
 			update(); // Update the view
@@ -465,12 +451,12 @@ public class GameView extends JFrame {
 	 * @author Nazifa Tanzim
 	 */
 	private void redo() {
-		if (!game.redo()) {
+		if(!game.redo()) {
 			displayMessage("No more redo's left");
-		} else {
+		} else{
 			update(); // Update the view
 		}
-
+		
 	}
 
 	/**
@@ -482,7 +468,7 @@ public class GameView extends JFrame {
 	public void displayMessage(String message) {
 		JOptionPane.showMessageDialog(this, message);
 	}
-
+	
 	/**
 	 * Listens for when user wants to start a new game
 	 * 
@@ -490,15 +476,6 @@ public class GameView extends JFrame {
 	 */
 	public void addNewGameListener(ActionListener a) {
 		newGameBtn.addActionListener(a);
-	}
-
-	/**
-	 * Listens for when a level has been selected for a new game
-	 * 
-	 * @param a
-	 */
-	public void addLevelListener(ActionListener a) {
-		startBtn.addActionListener(a);
 	}
 
 }
